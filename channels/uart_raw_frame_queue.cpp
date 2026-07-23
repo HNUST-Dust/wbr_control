c9e7811338cb3723b8fb6d155b0acbc7e2789bc5
NoneOfEver <noneofever@gmail.com>
2026-07-23 23:02:06 +0800

    fix:串口相关的通讯方式修改为控制线程独立维护

--- channels/uart_raw_frame_queue.cpp
+++ channels/uart_raw_frame_queue.cpp
@@ -2,15 +2,11 @@
 
 #include <channels/uart_raw_frame_queue.h>
 
-namespace channels::uart_raw_frame_queue {
+namespace channels {
 
 RING_BUF_DECLARE(remote_input_ring_buf, kRemoteInputRingBufSize);
 K_SEM_DEFINE(remote_input_sem, 0, 1);
-K_MSGQ_DEFINE(remote_input_uart_raw_msgq, sizeof(UartRawFrameMessage),
-              kQueueDepth, 4);
 K_MSGQ_DEFINE(referee_uart_raw_msgq, sizeof(UartRawFrameMessage), kQueueDepth,
               4);
-K_MSGQ_DEFINE(mavlink_uart_raw_msgq, sizeof(UartRawFrameMessage), kQueueDepth,
-              4);
 
-} // namespace channels::uart_raw_frame_queue
+} // namespace channels
--- channels/uart_raw_frame_queue.h
+++ channels/uart_raw_frame_queue.h
@@ -9,7 +9,7 @@
 #include <zephyr/kernel.h>
 #include <zephyr/sys/ring_buffer.h>
 
-namespace channels::uart_raw_frame_queue {
+namespace channels {
 
 constexpr size_t kUartRawChunkSize = 32U;
 constexpr size_t kQueueDepth = 32U;
@@ -22,10 +22,8 @@ struct UartRawFrameMessage {
 
 extern struct ring_buf remote_input_ring_buf;
 extern struct k_sem remote_input_sem;
-extern struct k_msgq remote_input_uart_raw_msgq;
 extern struct k_msgq referee_uart_raw_msgq;
-extern struct k_msgq mavlink_uart_raw_msgq;
 
-} // namespace channels::uart_raw_frame_queue
+} // namespace channels
 
 #endif /* RM_TEST_APP_CHANNELS_UART_RAW_FRAME_QUEUE_H_ */
